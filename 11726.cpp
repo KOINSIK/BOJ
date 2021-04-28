d278c46322708dcd8675b496105b16e13a4f1c1a
bbenipapa <72732950+KOINSIK@users.noreply.github.com>
2021-04-28 21:18:06 +0900

    [BOJ] 11726: 2×n 타일링

---
+++ 11726.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/11726
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, dp[1001];
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+	cout << dp[n] << '\n';
+	return 0;
+}
